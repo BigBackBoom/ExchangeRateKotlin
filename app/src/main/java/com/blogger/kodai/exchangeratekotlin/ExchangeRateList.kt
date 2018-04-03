5e9d134bd61083dc5ebbed354d424e1b7e6f8762
bigbackboom <kodai.kikuchi@uqconnect.edu.au>
2018-04-03 23:38:46 +0900

    Initial commit

---
+++ app/src/main/java/com/blogger/kodai/exchangeratekotlin/ExchangeRateList.kt
@@ -0,0 +1,12 @@
+package com.blogger.kodai.exchangeratekotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ExchangeRateList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exchange_rate_list)
+    }
+}
