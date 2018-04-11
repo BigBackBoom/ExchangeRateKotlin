c4433f7bed1ca7f609bb6a4935ac7ac112455ba6
bigbackboom <kodai.kikuchi@uqconnect.edu.au>
2018-04-11 17:25:40 +0900

    Added Layout

--- app/src/main/java/com/blogger/kodai/exchangeratekotlin/ExchangeRateList.kt
+++ app/src/main/java/com/blogger/kodai/exchangeratekotlin/ExchangeRateList.kt
@@ -2,11 +2,53 @@ package com.blogger.kodai.exchangeratekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 
 class ExchangeRateList : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d("ExchangeRateList", "onCreate")
+
         setContentView(R.layout.activity_exchange_rate_list)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ExchangeRateList", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.d("ExchangeRateList", "onResume")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.d("ExchangeRateList", "onRestart")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.d("ExchangeRateList", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.d("ExchangeRateList", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("ExchangeRateList", "onDestroy")
     }
 }
